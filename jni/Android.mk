4c3d5f8697c5dcf61440377205e21ee5773c9990
hetao <hetao@topeastic.com>
2016-05-17 19:23:56 +0800

    Android compile
    Change-Id: Ie8e8123a5291d53132d1440af610b9e6b1034c67

---
+++ jni/Android.mk
@@ -0,0 +1,39 @@
+#
+# Author Kevin He
+#
+#
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE    := devices
+
+SRC_ROOT := $(LOCAL_PATH)/../src
+
+ifeq ($(debug),1)
+LOCAL_CFLAGS += -DDEBUG
+endif
+
+LOCAL_CFLAGS := -O3 -DANDROID -DPLAT_LINUX -fexceptions
+
+LOCAL_C_INCLUDES := $(SRC_ROOT)/include $(SRC_ROOT)/devices/include
+
+#1 定义查找所有cpp文件的宏
+define all-cpp-files-under
+$(patsubst ./%,%, $(shell find $(SRC_ROOT) -name "*.cpp" -and -not -name ".*" -and -not -name "com_*"))
+endef
+
+define all-subdir-cpp-files
+$(call all-cpp-files-under,.)
+endef
+
+ALL_SRC_FILES := $(call all-subdir-cpp-files)
+LOCAL_SRC_FILES := $(patsubst %main.cpp,,$(ALL_SRC_FILES))
+
+$(warning LOCAL_SRC_FILES is $(LOCAL_SRC_FILES))
+
+LOCAL_SDK_VERSION := 9
+
+include $(BUILD_SHARED_LIBRARY)
